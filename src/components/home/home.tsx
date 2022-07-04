a78fa029893b6381f19e3c98c99223d08324b24b
Diogo Izele <diogogabrielizele@gmail.com>
2022-07-04 12:51:23 -0300

    feat(home): adds new image memoji in home

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,33 +1,68 @@
 import styles from "./home.module.scss";
+import Image from "next/image";
+
+import memojiImg from "../../assets/memoji-diogo-izele.webp";
 
 const Home = () => {
   return (
     <section className={styles.home}>
-      <h2
-        className={styles.hello}
-        data-aos="fade-down"
+      <div>
+        <h2
+          className={styles.hello}
+          data-aos="fade-down"
+          data-aos-duration="500"
+          data-aos-mirror="true"
+        >
+          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+            Hello, my name is
+          </span>
+        </h2>
+        <h1 data-aos="fade-right" data-aos-mirror="true">
+          <span className={styles.grow}>Diogo Izele</span>
+        </h1>
+        <h2 className={styles.role} data-aos="fade-left" data-aos-mirror="true">
+          I’m a{" "}
+          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+            Web Developer.
+          </span>
+        </h2>
+        <h3
+          className={styles.description}
+          data-aos="fade-up"
+          data-aos-duration="900"
+          data-aos-mirror="true"
+        >
+          I develop creative apps and websites, building the{" "}
+          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+            Frontend
+          </span>
+          ,{" "}
+          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+            Backend
+          </span>{" "}
+          and{" "}
+          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+            Mobile apps.
+          </span>
+        </h3>
+      </div>
+      <div
+        className={styles.memoji}
+        data-aos="fade-up"
         data-aos-duration="500"
         data-aos-mirror="true"
       >
-        <span className={styles["cian-detail"]}>Hello, my name is</span>
-      </h2>
-      <h1 data-aos="fade-right" data-aos-mirror="true">
-        <span>Diogo Izele</span>
-      </h1>
-      <h2 className={styles.role} data-aos="fade-left" data-aos-mirror="true">
-        I’m a <span className={styles["cian-detail"]}>Web Developer.</span>
-      </h2>
-      <h3
-        className={styles.description}
-        data-aos="fade-up"
-        data-aos-duration="900"
-        data-aos-mirror="true"
-      >
-        I develop creative apps and websites, building the{" "}
-        <span className={styles["cian-detail"]}>Frontend</span>,{" "}
-        <span className={styles["cian-detail"]}>Backend</span> and{" "}
-        <span className={styles["cian-detail"]}>Mobile apps.</span>
-      </h3>
+        <span className={styles.grow}>
+          <Image
+            src={memojiImg}
+            quality={100}
+            width={256}
+            height={256}
+            layout="fixed"
+            alt="Memoji - Apple character emoji about Diogo Izele"
+          />
+        </span>
+      </div>
     </section>
   );
 };
