68e926224d2c8a96468c027669a1523fe80af650
Diogo Izele <diogogabrielizele@gmail.com>
2022-07-05 11:34:42 -0300

    refactor(details): remove useless elements

--- src/containers/about/details/details.tsx
+++ src/containers/about/details/details.tsx
@@ -7,17 +7,20 @@ const Details = () => {
   const myCurrentAge = new Date().getFullYear() - YEAR_OF_BIRTH;
 
   return (
-    <div className={styles.details}>
-      <ul>
-        <DetailItem label="name">Diogo Gabriel Izele</DetailItem>
-        <DetailItem label="age">
-          <time dateTime="2002-03-23 00:00" title="March 23, 2002">
-            {myCurrentAge} years
-          </time>
-        </DetailItem>
-        <DetailItem label="location">Brazil - RS</DetailItem>
-      </ul>
-    </div>
+    <ul
+      className={styles.details}
+      data-aos="fade-down"
+      data-aos-duration="700"
+      data-aos-mirror="true"
+    >
+      <DetailItem label="name">Diogo Gabriel Izele</DetailItem>
+      <DetailItem label="age">
+        <time dateTime="2002-03-23 00:00" title="March 23, 2002">
+          {myCurrentAge} years
+        </time>
+      </DetailItem>
+      <DetailItem label="location">Brazil - RS</DetailItem>
+    </ul>
   );
 };
 
