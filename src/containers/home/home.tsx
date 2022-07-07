f1a465c30bc4c13ff371894b79103edcdcbf861c
Diogo Izele <diogogabrielizele@gmail.com>
2022-07-07 19:51:42 -0300

    refactor(home): updates some tags

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,43 +1,46 @@
 import styles from "./home.module.scss";
 import Image from "next/image";
-
 import memojiImg from "../../assets/memoji-diogo-izele.webp";
+
+import type { HoverEmphasisProps } from "./home.types";
+
+const HoverEmphasis = ({ className, children }: HoverEmphasisProps) => (
+  <em className={`${styles.emphasis} ${className}`}>{children}</em>
+);
 
 const Home = () => {
   return (
     <section className={styles.home}>
       <div>
         <p data-aos="fade-down" data-aos-duration="500" data-aos-mirror="true">
-          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+          <HoverEmphasis className={styles["cian-detail"]}>
             Hello, my name is
-          </span>
+          </HoverEmphasis>
         </p>
-        <h3
+        <h1
           data-aos="fade-right"
           data-aos-mirror="true"
           className={styles.name}
         >
-          <span className={styles.grow}>Diogo Izele</span>
-        </h3>
+          <HoverEmphasis>Diogo Izele</HoverEmphasis>
+        </h1>
         <h2 className={styles.role} data-aos="fade-left" data-aos-mirror="true">
           I&apos;m a{" "}
-          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+          <HoverEmphasis className={styles["cian-detail"]}>
             Web Developer.
-          </span>
+          </HoverEmphasis>
         </h2>
         <p data-aos="fade-up" data-aos-duration="900" data-aos-mirror="true">
           I develop creative apps and websites, building the{" "}
-          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+          <HoverEmphasis className={styles["cian-detail"]}>
             Frontend
-          </span>
+          </HoverEmphasis>
           ,{" "}
-          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
-            Backend
-          </span>{" "}
+          <HoverEmphasis className={styles["cian-detail"]}>Backend</HoverEmphasis>{" "}
           and{" "}
-          <span className={`${styles["cian-detail"]} ${styles.grow}`}>
+          <HoverEmphasis className={styles["cian-detail"]}>
             Mobile apps.
-          </span>
+          </HoverEmphasis>
         </p>
       </div>
       <div
