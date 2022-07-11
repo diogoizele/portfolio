5875a9639100923d3cddaf215211d31f7d4ea3c3
Diogo Izele <diogogabrielizele@gmail.com>
2022-07-11 12:14:57 -0300

    refactor: removes useless google fonts imported

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;600&display=swap"
             rel="stylesheet"
           />
         </Head>
