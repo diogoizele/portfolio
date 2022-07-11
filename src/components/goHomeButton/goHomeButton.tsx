56737e0b71afa7b1be27b14d66f89b4a1c5c32d1
Diogo Izele <diogogabrielizele@gmail.com>
2022-07-11 11:42:22 -0300

    refactor(go-home-button): removes border effect

--- src/components/goHomeButton/goHomeButton.tsx
+++ src/components/goHomeButton/goHomeButton.tsx
@@ -3,19 +3,17 @@ import styles from "./goHomeButton.module.scss";
 
 import { IoIosArrowUp } from "react-icons/io";
 
-const GoHomeButton = () => {
-  return (
-    <Link href="#home">
-      <div
-        className={styles["go-home-btn"]}
-        data-aos="slide-down"
-        data-aos-offset="800"
-        data-aos-duration="700"
-      >
-        <IoIosArrowUp />
-      </div>
-    </Link>
-  );
-};
+const GoHomeButton = () => (
+  <Link href="#home">
+    <div
+      className={styles["go-home-btn"]}
+      data-aos="slide-down"
+      data-aos-offset="800"
+      data-aos-duration="700"
+    >
+      <IoIosArrowUp />
+    </div>
+  </Link>
+);
 
 export default GoHomeButton;
