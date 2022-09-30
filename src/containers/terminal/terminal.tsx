94512d8b12b6931708178863929682bafa93c01b
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-30 18:14:28 -0300

    feat: creates terminal container

---
+++ src/containers/terminal/terminal.tsx
@@ -0,0 +1,40 @@
+import { TerminalCommand, TerminalResponse } from "components/index";
+import { useEffect, useState } from "react";
+
+import styles from "./terminal.module.scss";
+
+const TerminalHeader = () => {
+  return (
+    <header className={styles["terminal-header"]}>
+      <div className={styles["terminal-buttons"]}>
+        <div
+          className={`${styles["terminal-button"]} ${styles["terminal-button-close"]}`}
+        />
+        <div
+          className={`${styles["terminal-button"]} ${styles["terminal-button-minimize"]}`}
+        />
+        <div
+          className={`${styles["terminal-button"]} ${styles["terminal-button-maximize"]}`}
+        />
+      </div>
+      <span className={styles["terminal-title"]}>Terminal</span>
+    </header>
+  );
+};
+
+export const Terminal = () => {
+  return (
+    <section className={styles.terminal}>
+      <TerminalHeader />
+      <div className={styles["terminal-content"]}>
+        <TerminalCommand command="pwd" />
+        <TerminalResponse response="c:/diogoizele/portfolio" />
+        <TerminalCommand command="echo Hello, my name is Diogo Izele" />
+        <TerminalResponse response="Hello, my name is Diogo Izele" />
+        <TerminalCommand command="echo I'm a Web developer" />
+        <TerminalResponse response="I'm a Web developer" />
+        <TerminalCommand command="echo this is my portfolio" animated />
+      </div>
+    </section>
+  );
+};
