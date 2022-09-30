3aa448ce9996d48e3ac66920a2a714626043abcc
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-30 18:14:02 -0300

    feat: adds new font weights to project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;600;800;900&display=swap"
             rel="stylesheet"
           />
         </Head>
