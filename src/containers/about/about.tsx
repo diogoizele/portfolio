b6db578e46acd05fc75fad1f786845aa52acca60
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-30 18:16:57 -0300

    refactor: refactor about but removes in layout

--- src/containers/about/about.tsx
+++ src/containers/about/about.tsx
@@ -1,5 +1,3 @@
-import { GoHomeButton, Section } from "components";
-
 import styles from "./about.module.scss";
 
 import ResumeButton from "./resumeButton/resumeButton";
@@ -7,29 +5,15 @@ import Details from "./details/details";
 import Avatar from "./avatar/avatar";
 import AboutMe from "./aboutMe/aboutMe";
 
-const About = () => (
-  <Section title="About me" to="#about" id="about">
-    <div className={styles.about}>
-      <div className={styles["about__content--left"]}>
-        <Avatar />
-        <Details />
-        <ResumeButton />
-      </div>
-      <div className={styles["about__content--right"]}>
-        <AboutMe />
-        {/* <div className={styles.techs}>
-          <p>React</p>
-          <p>React Native</p>
-          <p>Next.js</p>
-          <p>Typescrips</p>
-          <p>Node</p>
-          <p>Java</p>
-          <p>Spring Boot</p>
-        </div> */}
-      </div>
+export const About = () => (
+  <div className={styles.about}>
+    <div className={styles["about__content--left"]}>
+      <Avatar />
+      <Details />
+      <ResumeButton />
     </div>
-    <GoHomeButton />
-  </Section>
+    <div className={styles["about__content--right"]}>
+      <AboutMe />
+    </div>
+  </div>
 );
-
-export default About;
