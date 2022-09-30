e4b8a724ca947a250bdde0787dc732e35272d776
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-30 18:12:56 -0300

    refactor: refactor header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,23 @@
 import Image from "next/image";
-import Nav from "components/nav/nav";
+
 import miniProfilePic from "assets/images/mini-profile-pic.webp";
 import styles from "./header.module.scss";
+import { Nav } from "..";
 
-const Header = () => (
-  <header
-    id="home"
-    className={styles.header}
-    data-aos="fade-down"
-    data-aos-offset="20"
-    data-aos-duration="300"
-    data-aos-easing="ease-in-sine"
-    data-aos-mirror="true"
-  >
-    <div className={styles["header__info"]}>
-      <Image
-        src={miniProfilePic}
-        alt="Diogo Izele's avatar"
-        width={48}
-        height={48}
-        quality={80}
-      />
-      <strong className={styles["info__name"]}>Diogo Izele</strong>
-    </div>
-    <Nav />
-  </header>
-);
-
-export default Header;
+export const Header = () => {
+  return (
+    <header id="home" className={styles.header}>
+      <div className={styles["header__info"]}>
+        <Image
+          src={miniProfilePic}
+          alt="Diogo Izele's avatar"
+          width={48}
+          height={48}
+          quality={90}
+        />
+        <strong className={styles["info__name"]}>Diogo Izele</strong>
+      </div>
+      <Nav />
+    </header>
+  );
+};
