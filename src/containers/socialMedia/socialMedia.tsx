a3ba1af876d4a1f3f56b57e10960d512e2a52f5f
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-13 17:35:54 -0300

    feat: adds youtube link in social media container

--- src/containers/socialMedia/socialMedia.tsx
+++ src/containers/socialMedia/socialMedia.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
+import { FiGithub, FiLinkedin, FiTwitter, FiYoutube } from "react-icons/fi";
 
 import styles from "./socialMedia.module.scss";
 
@@ -19,6 +19,11 @@ export const SocialMedia = () => {
       <Link href="https://twitter.com/diogoizele">
         <a target="_blank">
           <FiTwitter className={styles["social-media"]} size={28} />
+        </a>
+      </Link>
+      <Link href="https://www.youtube.com/channel/UCQ-oyN6ClB9W6-Z773iii9Q">
+        <a target="_blank">
+          <FiYoutube className={styles["social-media"]} size={28} />
         </a>
       </Link>
     </div>
