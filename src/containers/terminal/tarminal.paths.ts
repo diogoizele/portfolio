86e450036f64a8d5589183bd12a4e1d03f2d9eda
diogoizele <diogogabrielizele@gmail.com>
2022-10-27 15:56:08 -0300

    feat: adds routes as enums in terminal paths

--- src/containers/terminal/tarminal.paths.ts
+++ src/containers/terminal/tarminal.paths.ts
@@ -1,4 +1,23 @@
+export enum PATHS {
+  ROOT = "/",
+  HOME = "~",
+  PORTFOLIO = "~/portfolio",
+  BIN = "/bin",
+  DEV = "/dev",
+}
+
 export const terminalPaths = {
-  "/": "~",
-  "/portfolio": "/portfolio",
+  [PATHS.ROOT]: PATHS.ROOT,
+  [PATHS.HOME]: PATHS.HOME,
+  [PATHS.PORTFOLIO]: PATHS.PORTFOLIO,
+  [PATHS.BIN]: PATHS.BIN,
+  [PATHS.DEV]: PATHS.DEV,
+};
+
+export const pwdPaths = {
+  [PATHS.ROOT]: PATHS.ROOT,
+  [PATHS.HOME]: "/Users/diogoizele",
+  [PATHS.PORTFOLIO]: "/Users/diogoizele/portfolio",
+  [PATHS.BIN]: PATHS.BIN,
+  [PATHS.DEV]: PATHS.DEV,
 };
