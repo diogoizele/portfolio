736fdcafd45661ed2fbbdf838c9376d742289835
diogoizele <diogogabrielizele@gmail.com>
2022-10-27 15:53:20 -0300

    refactor: removes preloader at index importer

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,3 @@
 export { About } from "./about/about";
 export { Terminal } from "./terminal/terminal";
 export { SocialMedia } from "./socialMedia/socialMedia";
-export { Preloader } from "./preloader/preloader";
