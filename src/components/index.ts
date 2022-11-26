62bc974de6b25ed1b753c94064a66fab43c9e06c
diogoizele <diogogabrielizele@gmail.com>
2022-11-26 19:54:39 -0300

    feat(components): add easly export file index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Header } from "./header/header";
 export { TerminalCommand } from "./terminalCommand/terminalCommand";
 export { TerminalResponse } from "./terminalResponse/terminalResponse";
 export { ThemeSwitch } from "./themeSwitch/themeSwitch";
+export { default as Text } from "./text/text.component";
