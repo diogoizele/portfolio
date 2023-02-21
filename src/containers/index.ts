ea7fa22a5cca60778cd35dbe42652107cbc3bab6
diogoizele <diogogabrielizele@gmail.com>
2023-02-20 22:48:10 -0300

    feat: add education at containers index

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { Terminal } from "./terminal/terminal";
 export { SocialMedia } from "./socialMedia/socialMedia";
 export { Companies } from "./companies/companies.container";
+export { Education } from "./education/education.container";
