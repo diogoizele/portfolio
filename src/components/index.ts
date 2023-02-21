2877726394afea802bd51eef5b5682868750898b
diogoizele <diogogabrielizele@gmail.com>
2023-02-21 11:48:21 -0300

    refactor: remove old company card version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
 export { Header } from "./header/header";
 export { TerminalCommand } from "./terminalCommand/terminalCommand";
 export { TerminalResponse } from "./terminalResponse/terminalResponse";
-export { ThemeSwitch } from "./themeSwitch/themeSwitch";
+export { ThemeSwitch } from "./theme-switch/theme-switch.component";
 export { default as Text } from "./text/text.component";
-export { CompanyCard } from "./company-card/company-card.component";
 export { ResumeButton } from "./resume-button/resume-button.component";
 export { SpotifyCard } from "./spotify-card/spotify-card.component";
 export { ComponentItemCard } from "./company-item-card/component-item-card.component";
