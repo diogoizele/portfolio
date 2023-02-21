d27d5079a21ba8633f815f08030de37572f9e862
diogoizele <diogogabrielizele@gmail.com>
2023-02-21 11:49:01 -0300

    feat(contact): add project header to contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,15 @@
+import { ProjectHeader } from "../components";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <ProjectHeader
+        title="Diogo Izele - Contact"
+        description="How can you contact me"
+        keywords="Contact Diogo Izele"
+        url="https://diogoizele.com/contact"
+      />
+      <div>Contact</div>
+    </>
+  );
 }
