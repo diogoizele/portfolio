04feb06cf89b3813edb70da21f36ec369a4e659c
diogoizele <diogogabrielizele@gmail.com>
2023-08-07 21:33:51 -0300

    feat(resume): update resume file to syncronize with theme mode

--- src/components/resume-button/resume-button.component.tsx
+++ src/components/resume-button/resume-button.component.tsx
@@ -1,4 +1,7 @@
+import { useTheme } from "styled-components";
 import { HiOutlineDocumentDownload } from "react-icons/hi";
+
+import { URIS } from "config/constants";
 
 import {
   Container,
@@ -6,20 +9,25 @@ import {
   IconContainer,
   Text,
 } from "./resume-button.styles";
-export const ResumeButton = () => (
-  <Container>
-    <ButtonLink
-      href="https://github.com/diogoizele/DIOGO-IZELE-RESUME/raw/master/DIOGO-IZELE-RESUME.pdf"
-      download="DIOGO-IZELE-CURRICULUM"
-    >
-      <IconContainer>
-        <HiOutlineDocumentDownload
-          size={20}
-          title="Download Diogo Izele's Resume PDF"
-        />
-      </IconContainer>
 
-      <Text>Download Resume</Text>
-    </ButtonLink>
-  </Container>
-);
+export const ResumeButton = () => {
+  const { isDarkMode } = useTheme();
+  const { CURRICULUM } = URIS;
+
+  const curriculumLink = isDarkMode ? CURRICULUM.DARK : CURRICULUM.LIGHT;
+
+  return (
+    <Container>
+      <ButtonLink href={curriculumLink} download="DIOGO-IZELE-CURRICULUM">
+        <IconContainer>
+          <HiOutlineDocumentDownload
+            size={20}
+            title="Download Diogo Izele's Resume PDF"
+          />
+        </IconContainer>
+
+        <Text>Download Resume</Text>
+      </ButtonLink>
+    </Container>
+  );
+};
--- src/config/constants/uris.constants.ts
+++ src/config/constants/uris.constants.ts
@@ -3,4 +3,9 @@ export const URIS = {
     AUTH: "https://accounts.spotify.com/api/token",
     V1: "https://api.spotify.com/v1/me/player/currently-playing",
   },
+  CURRICULUM: {
+    DARK: "https://github.com/diogoizele/DIOGO-IZELE-RESUME/raw/master/dark-cv-model.pdf",
+    LIGHT:
+      "https://github.com/diogoizele/DIOGO-IZELE-RESUME/raw/master/light-cv-model.pdf",
+  },
 };
