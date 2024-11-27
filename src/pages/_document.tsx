454ee8eb7ef3cd22c62d71db1a480c84986261e3
Diogo Gabriel Izele <diogo.izele@cwi.com.br>
2024-11-27 19:48:06 -0300

    feat: add google adsense script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,6 +44,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;600;800;900&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3752079476360700"
+            crossorigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
